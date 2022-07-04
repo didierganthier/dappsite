e765ea460bcb2dcf155463571c5705f0f8010d7e
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-07-03 21:14:51 -0400

    post model class

---
+++ models/post.js
@@ -0,0 +1,16 @@
+export default class Post {
+    
+    id;
+    owner;
+    price;
+    text;
+    originalOwner;
+
+    constructor(data) {
+        this.originalOwner = data[0];
+        this.price = data[1];
+        this.text = data[2];
+        this.id = data[3];
+        this.owner = data[4];
+    }
+}
