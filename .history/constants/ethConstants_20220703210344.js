185bc0e074ec39865eb3b05c2a5e902bf5e73f4c
Didier Peran Ganthier <didierganthierperan@gmail.com>
2022-07-03 21:13:45 -0400

    ethEndpoint and address added

---
+++ .history/constants/ethConstants_20220703210344.js
@@ -0,0 +1,2 @@
+export const ethEndpoint = "HTTP://127.0.0.1:7545";
+export const contractAddress = "YOUR CONTRACT ADDRESS";
---
+++ .history/constants/ethConstants_20220703211331.js
@@ -0,0 +1,3 @@
+export const ethEndpoint = "HTTP://127.0.0.1:7545";
+export const contractAddress = "0xD5ceD280d81dC7c37af0d8C1ECdB99f6b7Eb75Dc";
+
